efd32ade96118dab1e299bba55bb2ac9113f00e0
MatveyMironov <matveymironof@gmail.com>
2024-10-06 18:48:22 +0300

    Camera follows player

---
+++ TinyCreatures/Assets/_Presentation/Features/Player/Player Camera/CameraFollowPlayer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    private void Update()
+    {
+        Vector3 targetPosition = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = targetPosition;
+    }
+}
